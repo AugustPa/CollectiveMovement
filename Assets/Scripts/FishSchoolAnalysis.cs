e004244fc406e3e9ff6b5f60f0c11527f8941c7b
AugustPaula <101798634+AugustPa@users.noreply.github.com>
2023-12-06 19:04:47 +0100

    added rotation and polarization plot

--- Assets/Scripts/FishSchoolAnalysis.cs
+++ Assets/Scripts/FishSchoolAnalysis.cs
@@ -6,12 +6,15 @@ public class FishSchoolAnalysis : MonoBehaviour
     public List<Fish> fishSchool; // Reference to the fish school
 
     private Vector3 centerOfMass;
-    private Vector3 rotation;
+    private float rotation;
     private float polarization;
+    private float plotSize = 250;
 
     public Vector3 CenterOfMass => centerOfMass;
-    public Vector3 Rotation => rotation;
+    public float Rotation => rotation;
     public float Polarization => polarization;
+    public RectTransform pointMarker;
+
 
     void Update()
     {
@@ -23,6 +26,11 @@ public class FishSchoolAnalysis : MonoBehaviour
         Debug.Log("Center of Mass: " + centerOfMass);
         Debug.Log("Rotation: " + rotation);
         Debug.Log("Polarization: " + polarization);
+        // Map rotation and polarization to graph positions
+
+        // Map rotation and polarization to graph positions
+        Vector2 markerPos = MapToGraph(rotation, polarization);
+        pointMarker.anchoredPosition = markerPos;
     }
 
     private Vector3 CalculateCenterOfMass()
@@ -41,17 +49,26 @@ public class FishSchoolAnalysis : MonoBehaviour
 }
 
 
-    private Vector3 CalculateRotation(Vector3 centerOfMass)
+    private float CalculateRotation(Vector3 centerOfMass)
     {
         Vector3 sumRotation = Vector3.zero;
+        int fishCount = fishSchool.Count;
+
         foreach (Fish fish in fishSchool)
         {
-            Vector3 directionVector = (fish.transform.position - centerOfMass).normalized;
-            Vector3 velocityUnitVector = fish.velocity.normalized;
-            sumRotation += Vector3.Cross(velocityUnitVector, directionVector);
+            Vector3 directionToCenterOfMass = (fish.transform.position - centerOfMass).normalized;
+            Vector3 fishDirection = fish.velocity.normalized;
+            
+            Vector3 crossProduct = Vector3.Cross(fishDirection, directionToCenterOfMass);
+            sumRotation += crossProduct;
         }
-        return sumRotation;
+
+        // Calculate the average of the sum of the absolute values of the cross products
+        float averageRotation = sumRotation.magnitude / fishCount;
+
+        return averageRotation; // This value should already be between 0 and 1
     }
+
 
     private float CalculatePolarization()
     {
@@ -62,4 +79,13 @@ public class FishSchoolAnalysis : MonoBehaviour
         }
         return (sumVelocities / fishSchool.Count).magnitude;
     }
+    private Vector2 MapToGraph(float rotationValue, float polarizationValue)
+    {
+        // Assume graphSize and maxValue are defined to represent the size of your graph and the max values you expect
+        float x = (polarizationValue) * plotSize - (plotSize / 2); // Centering the graph
+        float y = (rotationValue) * plotSize - (plotSize / 2);
+
+        return new Vector2(x, y);
+    }
+
 }
